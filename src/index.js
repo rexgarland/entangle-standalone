4fb748020ee8ff34b2f0c0d678c49dd8cd8304f1
Rex Garland <rex.garland@gmail.com>
2022-01-10 12:30:56 -0600

    Publishing as a package

--- src/index.js
+++ src/index.js
@@ -4,53 +4,54 @@ import { parse } from "./parse.js";
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-import yargs from 'yargs'
-import {hideBin} from 'yargs/helpers'
-var argv = yargs(hideBin(process.argv)).argv
-const mangleFile = argv._[0]
+import path from "path";
+import Handlebars from "handlebars";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-import path from "path";
+function build(mangleFile, outFile) {
 
-import Handlebars from "handlebars";
+	const mangle = fs.readFileSync(mangleFile, "utf8");
+	const { markup, code } = parse(mangle);
 
-const mangle = fs.readFileSync(mangleFile, "utf8");
-const { markup, code } = parse(mangle);
-
-// render js
-const jsSource = fs.readFileSync(
-	path.join(__dirname, "tangle/example.template.js"),
-	"utf8"
-);
-const jsTemplate = Handlebars.compile(jsSource);
-const js = jsTemplate(code);
-fs.writeFileSync(path.join(__dirname, "tangle/example.js"), js, "utf8");
+	// render js
+	const jsSource = fs.readFileSync(
+		path.join(__dirname, "tangle/example.template.js"),
+		"utf8"
+	);
+	const jsTemplate = Handlebars.compile(jsSource);
+	const js = jsTemplate(code);
+	fs.writeFileSync(path.join(__dirname, "tangle/example.js"), js, "utf8");
 
 
-// browserify
+	// browserify
 
-import browserify from 'browserify'
-var b = browserify()
-b.add(path.join(__dirname, 'tangle/example.js'))
-var out = fs.createWriteStream('dist/bundle.js', 'utf8')
-b.bundle().pipe(out)
+	import browserify from 'browserify'
+	var b = browserify()
+	b.add(path.join(__dirname, 'tangle/example.js'))
+	var out = fs.createWriteStream('dist/bundle.js', 'utf8')
+	b.bundle().pipe(out)
 
-out.on('finish', () => {
+	out.on('finish', () => {
 
-	// get template
-	const html = fs.readFileSync(path.join(__dirname, 'tangle/example.template.html'), 'utf8');
-	const template = Handlebars.compile(html);
+		// get template
+		const html = fs.readFileSync(path.join(__dirname, 'tangle/example.template.html'), 'utf8');
+		const template = Handlebars.compile(html);
 
-	// get data
-	const data = {
-		style: fs.readFileSync(path.join(__dirname, 'tangle/example.css'), 'utf8'),
-		tangleKitStyle: fs.readFileSync(path.join(__dirname, 'tangle/TangleKit/TangleKit.css'), 'utf8'),
-		markup,
-		script: fs.readFileSync('dist/bundle.js', 'utf8')
-	}
+		// get data
+		const data = {
+			style: fs.readFileSync(path.join(__dirname, 'tangle/example.css'), 'utf8'),
+			tangleKitStyle: fs.readFileSync(path.join(__dirname, 'tangle/TangleKit/TangleKit.css'), 'utf8'),
+			markup,
+			script: fs.readFileSync('dist/bundle.js', 'utf8')
+		}
 
-	// save
-	const output = template(data);
-	fs.writeFileSync('dist/document.html', output, 'utf8');
-})
+		// save
+		const output = template(data);
+		fs.writeFileSync(outFile, output, 'utf8');
+	})
+}
+
+module.exports = {
+	build
+}
